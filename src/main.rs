8308d4e9cf7014aeaf32ce7953cdbdac4ba10532
Caden Miller <mcaden966@gmail.com>
2021-07-26 22:24:02 -0500

    population runs for set number of generations

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,23 @@ pub mod frozen_lake;
 pub mod snake;
 
 use crate::population::Population;
-use crate::snake::Snake;
 
 fn main() {
-	/*
 	let mut population = Population::new(vec![14, 4], 50);
+	let mut solved_generation = 0;
+	let mut has_solved = false;
 
-	while !population.get_has_solved() {
+	while population.get_generation() < 100 {
 		population.calc_fitness();
 		println!("Generation {}: af = {}", population.get_generation(), population.get_average_fitness());
 		population.produce_new_gen();
+		if population.get_has_solved() && !has_solved {
+			has_solved = true;
+			solved_generation = population.get_generation();
+		}
 	}
 
-	println!("Solved in {} generations.", population.get_generation());
+	println!("Solved in {} generations.", solved_generation);
 	population.play_best_individual();
 	//population.print_best_individual();
-	*/
-
-	let mut snake = Snake::new();
-	snake.play_game();
 }
